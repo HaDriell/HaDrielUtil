8237a9db7d7abf2ec10a33bc2d5b6a441573cf63
glathuiliere <gautierlathu@hotmail.fr>
2016-08-19 17:16:47 +0200

    Weird naming bug with JsPrimitive

---
+++ src/fr/hadriel/serialization/JsPrimitive.java
@@ -0,0 +1,52 @@
+package fr.hadriel.serialization;
+
+import fr.hadriel.serialization.json.Json;
+import fr.hadriel.serialization.json.JsonType;
+
+/**
+ * Created by glathuiliere on 19/08/2016.
+ */
+public class JsPrimitive implements JsonType {
+
+    private Object value;
+
+    public JsPrimitive(Object value) {
+        set(value);
+    }
+
+    public void set(Object value) {
+        Json.checkJsonValidPrimitiveType(value);
+        this.value = value;
+    }
+
+    public Object get() {
+        return value;
+    }
+
+    public boolean isNull() {
+        return value == null;
+    }
+
+    public boolean isBool() {
+        return value instanceof Boolean;
+    }
+
+    public boolean isNumber() {
+        return value instanceof Byte ||
+                value instanceof Short ||
+                value instanceof Character ||
+                value instanceof Integer ||
+                value instanceof Long ||
+                value instanceof Float ||
+                value instanceof Double;
+    }
+
+    public boolean isString() {
+        return value instanceof String;
+    }
+
+    public String toString() {
+        if(isNull()) return "null";
+        return isString() ? '"' + value.toString() + '"' : value.toString();
+    }
+}
